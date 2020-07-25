070de2b7c797549b658e784d125534eada525208
Nathan <ncpenn@gmail.com>
2020-07-25 14:03:17 -0500

    changing due to Poshmark redoing their UI

--- auto_share.js
+++ auto_share.js
@@ -1,24 +1,27 @@
-(function($) {
-    const ajaxSuccessEvent = "lprequestend";
+(function() {
     const inventoryTagClass = ".inventory-tag";
-    const shareButtonClass = ".share";
-    const shareModalId = "#share-popup";
-    const followerShareClass = ".pm-followers-share-link";
-    const randomMilliseconds = () => Math.floor(Math.random() * (3000 - 1070)) + 1070;
+    const shareButtonClass = ".social-action-bar__share";
+    const shareModalId = ".share-modal";
+    const followerShareClass = ".internal-share__link";
+    const randomMilliseconds = () => Math.floor(Math.random() * (3010 - 1570)) + 1570;
     const isVisible = el => el.offsetParent !== null || getComputedStyle(el).display !== "none";
     const getCaptchaElement = () => document.querySelector("#captcha-popup");
     const getWindowHeight = () => document.body.offsetHeight;
-    const scrollToBottomOfPage = () => window.scrollTo(0, getWindowHeight());
+    const scrollToBottomOfPage = () => window.scrollTo(0, document.body.offsetHeight);
     const getAllTiles = () => document.querySelectorAll(".tile");
     const getActiveTiles = () => {
         const allTiles = getAllTiles();
         window.console.log("You have a total of " + allTiles.length + " items in your closet.");
         return Array.prototype.filter.call(allTiles, tile => tile.querySelector(inventoryTagClass) === null)
     };
+    const clickModalShareButton = () => {
+        document
+        .querySelector(shareModalId)
+        .querySelector(followerShareClass)
+        .click();
+    };
     const getShareButton = t => t.querySelector(shareButtonClass);
     const shareActiveListings = () => {
-        const shareModal = document.querySelector(shareModalId);
-        const shareToFollowersButton = shareModal.querySelector(followerShareClass);
         const activeTiles = getActiveTiles();
         let currentTileIndex = 0;
         let captchaEl = getCaptchaElement();
@@ -27,30 +30,45 @@
             if (!captchaEl || !isVisible(captchaEl)) {
                 window.console.log("sharing item: " + currentTileIndex + " of " + activeTiles.length + " active listings.");
                 const currentTile = activeTiles[currentTileIndex++];
-                const shareButton = getShareButton(currentTile);
-                shareButton.click();
-                shareToFollowersButton.click();
-            }
-            if (currentTileIndex < activeTiles.length) {
-                window.setTimeout(shareNextActiveTile, randomMilliseconds());
+                const shareButton = getShareButton(currentTile); 
+                shareButton.click(clickModalShareButton);
+                let isModalShowing = setInterval(function(){
+                   if(isVisible(document.querySelector(shareModalId)) && isVisible(document.querySelector(followerShareClass))) {
+                        clearInterval(isModalShowing);
+                        clickModalShareButton();
+
+                        let isSuccessToastGone = setInterval(function(){
+                            if(!isVisible(document.querySelector('#flash__message'))){
+                                clearInterval(isSuccessToastGone);
+                                
+                                if (currentTileIndex < activeTiles.length) {
+                                    window.setTimeout(shareNextActiveTile, randomMilliseconds());
+                                }
+                            }
+                        }, 100);  
+                   }
+                }, 100);
             }
         };
         shareNextActiveTile();
     };
-    let lastWindowHeight = getWindowHeight();
-    const checkHeightAndScroll = () => {
-        const newHeight = getWindowHeight();
-        if (newHeight !== lastWindowHeight) {
-            lastWindowHeight = newHeight;
+
+    let lastHeight = getWindowHeight();
+    let numberOfTimesScollToBottomFailed = 0
+    scrollToBottomOfPage();
+    let handler = setInterval(function(){
+        let current = getWindowHeight();
+        if(lastHeight != current) {
+            lastHeight = current;
             scrollToBottomOfPage();
+            numberOfTimesScollToBottomFailed = 0;
         } else {
-            $(document).off("ajaxComplete");
+            numberOfTimesScollToBottomFailed++;
+        }
+
+        if(numberOfTimesScollToBottomFailed > 10){
+            clearInterval(handler);
             shareActiveListings();
         }
-    };
-    $(document).ajaxComplete(checkHeightAndScroll);
-    scrollToBottomOfPage();
-    if (Number(document.querySelectorAll(".active a .count")[0].innerText) < 49) {
-        checkHeightAndScroll();
-    }
-})(jQuery);
+    }, 500);
+})();
